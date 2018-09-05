4e2b065b4bfc7125d874e9669b65761837b9545e
yundianzixun <zhenghhgz@163.com>
2018-09-05 17:09:19 +0800

    first commit

---
+++ src/main/java/com/itunion/springbootstarter/SpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package com.itunion.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterApplication.class, args);
+	}
+}
